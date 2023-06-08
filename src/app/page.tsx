9d9f744586d8cf8b7c0b83701a02f0de90fd08ea
Rajib Ali Shah <121859284+RajibAliShah@users.noreply.github.com>
2023-06-08 04:40:23 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import About from "./components/about"
+// import Contact from "./components/Contact"
+// import Header from "./components/Header"
+// import Intro from "./components/Intro"
+// import Portfolio from "./components/Portfolio"
+// import Skills from "./components/Skills"
+// import Socialicons from "./components/Socialicons"
+
+
+export default function Home() {
+  return (
+  <>
+  {/* <Socialicons />
+  <Header />
+  <Intro /> */}
+  <About />
+  {/* <Portfolio />
+  <Skills />
+  <Contact />
+   */}
+   </>
+  )
+}
